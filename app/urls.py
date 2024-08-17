1b0fc9902dda32d7b6d833ca6a8096ecb413130c
rohityadav22470 <rohityadav6767123@gmail.com>
2024-08-17 23:41:03 +0530

    Basic implementation completed

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from . import views
 
+app_name ="app"
 urlpatterns = [
     path('',views.index,name='index'),
     path("<int:question_id>/",views.detail, name='detail'),
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question,choice
+from django.db.models import F
 from django.urls import reverse
 
 # Create your views here.
@@ -26,14 +27,14 @@ def results(request, question_id):
 
 
 
-def vote(request,question_id):
+def vote(request, question_id):
     question =get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice=choice.choice_set.get(pk=request.POST["choice"])
+        selected_choice=question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, choice.DoesNotExist):
         return render(request, 'detail.html', {"question":question, "error_message": "You didn't selected a choice"})
     else:
         selected_choice.votes =F('votes')+1
         selected_choice.save()
-    return HttpResponseRedirect(reverse('results', args=(question_id)))
+    return HttpResponseRedirect(reverse('app:results', args=(question_id,)))
     
