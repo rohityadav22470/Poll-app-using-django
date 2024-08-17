85f920d50239a8b08b74ec56fd83029d650f893f
rohityadav22470 <rohityadav6767123@gmail.com>
2024-08-17 15:20:20 +0530

    application added

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question,choice
+
+# Create your views here.
+def index(request):
+    questions =Question.objects.order_by("-pub_date")[:5]
+    output=", ".join([q.question for q in questions])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse('The question Id of the question is %s' %question_id)
+ 
+def results(request, question_id):
+    response ="You're looking at the result of question id %s"
+    return HttpResponse(response %question_id)
+
+def vote(request,question_id):
+    return HttpResponse('You are voting on question %s' %question_id)
