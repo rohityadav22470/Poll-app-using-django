619ea2dd40c88296e16b9f275259449539a31d2a
rohityadav22470 <rohityadav6767123@gmail.com>
2024-08-17 16:47:17 +0530

    Details.html file added and views updated

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Question,choice
 
 # Create your views here.
@@ -8,12 +8,22 @@ def index(request):
     q = {"questions": questions}
     return render(request, "index.html",q)
 
+
 def detail(request, question_id):
-    return HttpResponse('The question Id of the question is %s' %question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question Id does not exist.")
+    # question=get_object_or_404(Question, pk=question_id)...this is the shortcut for try and except 
+    return render(request,"detail.html", {"question":question})
  
+
+
 def results(request, question_id):
     response ="You're looking at the result of question id %s"
     return HttpResponse(response %question_id)
+
+
 
 def vote(request,question_id):
     return HttpResponse('You are voting on question %s' %question_id)
