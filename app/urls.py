19daeedfeb569652a0a3f23869ed96cbeb645be5
rohityadav22470 <rohityadav6767123@gmail.com>
2024-08-18 02:11:08 +0530

    adding testcases for testing application and applied boundary conditions

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 app_name ="app"
 urlpatterns = [
-    path('',views.index,name='index'),
-    path("<int:question_id>/",views.detail, name='detail'),
-    path("<int:question_id>/result/",views.results,name='results'),
+    # path('',views.index,name='index'),
+    # path("<int:question_id>/",views.detail, name='detail'),
+    # path("<int:question_id>/result/",views.results,name='results'),
+    path('',views.IndexView.as_view(),name='index'),
+    path("<int:pk>/",views.DetailView.as_view(), name='detail'),
+    path("<int:pk>/result/",views.ResultView.as_view(),name='results'),
     path('<int:question_id>/result/vote/',views.vote, name="vote")
 ]
--- app/views.py
+++ app/views.py
@@ -3,28 +3,46 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question,choice
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
 
 # Create your views here.
-def index(request):
-    questions =Question.objects.order_by("-pub_date")[:5]
-    q = {"questions": questions}
-    return render(request, "index.html",q)
+# def index(request):
+#     questions =Question.objects.order_by("-pub_date")[:5]
+#     q = {"questions": questions}
+#     return render(request, "index.html",q)
 
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question Id does not exist.")
-    # question=get_object_or_404(Question, pk=question_id)...this is the shortcut for try and except 
-    return render(request,"detail.html", {"question":question})
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question Id does not exist.")
+#     # question=get_object_or_404(Question, pk=question_id)...this is the shortcut for try and except 
+#     return render(request,"detail.html", {"question":question})
  
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'result.html',{'question':question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'result.html',{'question':question})
 
+
+class IndexView(generic.ListView):
+    template_name="index.html"
+    context_object_name= "questions"
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+class DetailView(generic.DetailView):
+    template_name = "detail.html"
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
+  
+class ResultView(generic.DetailView):
+    model = Question
+    template_name="result.html"
 
 
 def vote(request, question_id):
