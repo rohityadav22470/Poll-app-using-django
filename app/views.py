cf391982c6b3868f71df425e096a2e23e60af4e6
rohityadav22470 <rohityadav6767123@gmail.com>
2024-08-20 23:36:25 +0530

    authentication system implemented

--- app/views.py
+++ app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-from .models import Question,choice
+from .models import *
 from django.db.models import F
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 # def index(request):
@@ -29,7 +31,7 @@ from django.contrib import messages
 #     question = get_object_or_404(Question, pk=question_id)
 #     return render(request, 'result.html',{'question':question})
 
-
+# @login_required
 class IndexView(generic.ListView):
     template_name="index.html"
     context_object_name= "questions"
@@ -58,27 +60,81 @@ def vote(request, question_id):
     return HttpResponseRedirect(reverse('app:results', args=(question_id,)))
     
     # User auth
-def login(request):
-     
-    return render(request,"login.html")
+# def login(request):
+#     if request.method=="POST":
+#         username=request.POST.get("username")
+#         password=request.POST.get("password")
+#         if User.objects.filter(username=username).exists():
+
+
+
+#     return render(request,"login.html")
 
 def register(request):
     if request.method=="POST":
-        username=request.POST["username"]
-        password=request.POST["password"]
-        person=request.POST["first_name"]
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        person=request.POST.get("first_name")
         user=User.objects.filter(username=username)
         if user.exists():
             messages.error(request, "USERNAME ALREADY EXISTS. WRITE ANOTHER")
-            return redirect(request, "register.html")
-        
-        user=User.objects.create(
+            return redirect("/register/")
+        user=User.objects.create_user(
             username=username,
             first_name=person
             )
-        
         user.set_password(password) #for encrypted password
         user.save()
         messages.success(request, 'Account created successfully!')
-        return redirect(request,"login.html")
+        return redirect("/login/")
     return render(request,"register.html")
+
+
+# jhhj
+# def register(request):
+#     if request.method=='POST':
+#         username=request.POST['username']
+#         email=request.POST['email']
+#         pass1=request.POST['password']
+#         pass2=request.POST['password1']
+#         if pass1==pass2:
+#             if  User.objects.filter(email=email).exists():
+#                 messages.info(request,'Email already Used')
+#                 return redirect('register')
+#             elif  User.objects.filter(username=username).exists():
+#                 messages.info(request,'username already exists')
+#                 return redirect('register')
+#             else:
+#                 user= User.objects.create_user(username= username, email=email, password=pass1)
+#                 user.save()
+#                 return redirect('login')
+#     else:
+#         messages.info(request, 'Password not same.')
+#         return redirect('register')
+    
+#     return render(request, 'register.html')
+
+# # for new login
+def login_page(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(username=username, password=password)
+
+        # if request.POST['register']:
+        #     return redirect('register')
+        if user is not None:
+            auth.login(request, user)
+            person=User.objects.filter(username=username)
+            print(person)
+            return render(request,'index.html', {"username": person})
+        else:
+            messages.info(request, "INVALID CREDENTIALS!!!")
+            return redirect('login')
+
+    return render(request,'login.html')
+
+# # for logout functionality
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
